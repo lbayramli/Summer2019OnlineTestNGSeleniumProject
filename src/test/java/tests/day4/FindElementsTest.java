809b5d70bf32009d4be8c77d2b814f9f867a4a54
Leyla Bayramli <lbayramli92@gmail.com>
2019-10-22 12:36:40 -0700

    day4

--- src/test/java/tests/day4/FindElementsTest.java
+++ src/test/java/tests/day4/FindElementsTest.java
@@ -10,12 +10,21 @@ public class FindElementsTest {
     public static void main(String[] args) {
         WebDriver driver= BrowserFactory.getDriver("chrome");
         driver.get("http://practice.cybertekschool.com/forgot_password");
+        String expectedResult=driver.getTitle();
         //STEP1. Open instructor in chrome and find locater for the elemenet
         //Step 2. Create object of Webelement
         //Step3: Use Webelement
         WebElement button=driver.findElement(By.id("form_submit"));
         //to click to the element
         button.click();
+        String actualResult=driver.getTitle();
+       if(actualResult.equals(expectedResult)){
+           System.out.println("Test Passed");
+       }else{
+           System.out.println("Test Failed");
+           System.out.println("Expected Title: "+expectedResult);
+           System.out.println("Actual Result: "+ actualResult);
+       }
         BrowserUtils.wait(2);
         driver.close();
     }
